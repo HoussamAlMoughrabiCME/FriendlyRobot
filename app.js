1e82b72ef1c12509e1fc233121587799e40f543e
houssam <houssam.almoughrabi@gotocme.com>
2016-09-16 16:53:29 +0300

    add plans2

--- app.js
+++ app.js
@@ -591,31 +591,34 @@ function sendButtonMessage(recipientId) {
          payload: {
            template_type: "generic",
            elements: [{
-             title: "3 Day 80 MB",
-             subtitle: "80.00 JMD",
-             item_url: "#",
-             //image_url: SERVER_URL + "/assets/rift.png",
-             image_url:"https://product-staging.digicelgroup.com/selfcare3/img/digicelsportsmax_promo.png",
+             title: "rift",
+             subtitle: "Next-generation virtual reality",
+             item_url: "https://www.oculus.com/en-us/rift/",
+             image_url: SERVER_URL + "/assets/rift.png",
              buttons: [{
+               type: "web_url",
+               url: "https://www.oculus.com/en-us/rift/",
+               title: "Open Web URL"
+             }, {
                type: "postback",
-               title: "Buy",
+               title: "Call Postback",
                payload: "Payload for first bubble",
              }],
-           },
-           {
-             title: "3 Day 80 MB",
-             subtitle: "80.00 JMD",
-             item_url: "#",
-             //image_url: SERVER_URL + "/assets/rift.png",
-             image_url:"https://product-staging.digicelgroup.com/selfcare3/img/digicelsportsmax_promo.png",
+           }, {
+             title: "touch",
+             subtitle: "Your Hands, Now in VR",
+             item_url: "https://www.oculus.com/en-us/touch/",
+             image_url: SERVER_URL + "/assets/touch.png",
              buttons: [{
+               type: "web_url",
+               url: "https://www.oculus.com/en-us/touch/",
+               title: "Open Web URL"
+             }, {
                type: "postback",
-               title: "Buy",
-               payload: "Payload for first bubble",
-             }],
-           }
-
-         ]
+               title: "Call Postback",
+               payload: "Payload for second bubble",
+             }]
+           }]
          }
        }
      }
